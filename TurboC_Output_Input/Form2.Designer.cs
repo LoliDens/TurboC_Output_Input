a3666c40ba790cb6685d92dd75a4aee9e64d6c0d
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-12-10 19:39:08 +0500

    Нухуйы

--- TurboC_Output_Input/Form2.Designer.cs
+++ TurboC_Output_Input/Form2.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -64,11 +65,22 @@
             this.textBox1.Size = new System.Drawing.Size(534, 22);
             this.textBox1.TabIndex = 3;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(572, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -85,5 +97,6 @@
         private Button button1;
         private Button button2;
         private TextBox textBox1;
+        private Button button3;
     }
 }
